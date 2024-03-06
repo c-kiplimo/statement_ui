b905185174ac1d513fa0b7b3d2f34055852cd49b
Daniel Njoroge <dank8505@gmail.com>
2024-03-06 17:10:23 +0300

    Added Configurable properties to Balance Info Card

--- src/components/widgets/balance-info-card/balance.info.card.tsx
+++ src/components/widgets/balance-info-card/balance.info.card.tsx
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 import styles from './balance.info.card.module.css';
-import { svgIcon } from '../../atoms/imageIcon/imageIcon';
 import VerticalInfoDescription from '../../atoms/text/vertical-info-description';
 import { EyeOutlined } from '@ant-design/icons';
 import ImageIcon from '../../atoms/imageIcon/imageIcon';
 
-const currencies=
-  {
-    currencyTitle:'Kenya Shillings',
-    availableBalance:'2344',
-    currencyCode:'KES'
-  }
-
 type clikCard={
-  onClick:(event:any)=>{}
+  svgImage:ReactNode;
+  currencyTitle:String;
+  balanceDescription:String;
+  availableBalance: String;
+  currencyCode:String;
+  onClick?:(event:any)=>{}
 }
 
 function BalanceInfoCard(props:clikCard) {
@@ -25,13 +22,13 @@ function BalanceInfoCard(props:clikCard) {
   return (
     <div className={styles.container} onClick={props.onClick}>
         <div className={styles.icondiv}>
-            <div className={styles.icon}> <ImageIcon icon={svgIcon} /></div>
-            <div className={styles.description}><VerticalInfoDescription  title={currencies.currencyTitle} description='Available balance'/> </div>
+            <div className={styles.icon}> <ImageIcon icon={props.svgImage} /></div>
+            <div className={styles.description}><VerticalInfoDescription  title={props.currencyTitle} description={props.balanceDescription}/> </div>
         </div>
 
         <div className={styles.eyeContent}>
           <div className={isVisible ? styles.displayContent : styles.hideContent}>
-            <VerticalInfoDescription title={currencies.availableBalance} description={currencies.currencyCode} titleStyle={{fontWeight:'bold'}}/>
+            <VerticalInfoDescription title={props.availableBalance} description={props.currencyCode} titleStyle={{fontWeight:'bold'}}/>
           </div>
           <div className={styles.eyeicon}>
             <EyeOutlined onClick={handleClick} style={{}}/>
