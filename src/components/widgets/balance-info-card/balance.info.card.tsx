2553e3f5774766a79581d951d01639a414370284
Daniel Njoroge <dank8505@gmail.com>
2024-03-06 14:58:38 +0300

    Added balance info card properties

--- src/components/widgets/balance-info-card/balance.info.card.tsx
+++ src/components/widgets/balance-info-card/balance.info.card.tsx
@@ -2,28 +2,36 @@ import React, { useState } from 'react';
 import styles from './balance.info.card.module.css';
 import { svgIcon } from '../../atoms/imageIcon/imageIcon';
 import VerticalInfoDescription from '../../atoms/text/vertical-info-description';
-import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
+import { EyeOutlined } from '@ant-design/icons';
 import ImageIcon from '../../atoms/imageIcon/imageIcon';
 
+const currencies=
+  {
+    currencyTitle:'Kenya Shillings',
+    availableBalance:'2344',
+    currencyCode:'KES'
+  }
 
-function BalanceInfoCard() {
+type clikCard={
+  onClick:(event:any)=>{}
+}
+
+function BalanceInfoCard(props:clikCard) {
   const [isVisible, setIsVisible] = useState(false);
   
-
   function handleClick() {
     setIsVisible(!isVisible);
   }
-  
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={props.onClick}>
         <div className={styles.icondiv}>
             <div className={styles.icon}> <ImageIcon icon={svgIcon} /></div>
-            <div className={styles.description}><VerticalInfoDescription  title='Kenyan Shillings' description='Available balance' titleStyle={{color:'#151E00', fontSize:'16px', fontWeight:'400' }} descriptionStyle={{color:'#6F7269', fontSize:'12px', fontWeight:'400'}}/> </div>
+            <div className={styles.description}><VerticalInfoDescription  title={currencies.currencyTitle} description='Available balance'/> </div>
         </div>
 
         <div className={styles.eyeContent}>
           <div className={isVisible ? styles.displayContent : styles.hideContent}>
-            <VerticalInfoDescription title='354,500.00' description='KES' titleStyle={{fontWeight:'bold'}}/>
+            <VerticalInfoDescription title={currencies.availableBalance} description={currencies.currencyCode} titleStyle={{fontWeight:'bold'}}/>
           </div>
           <div className={styles.eyeicon}>
             <EyeOutlined onClick={handleClick} style={{}}/>
