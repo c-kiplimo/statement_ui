35b4e6322f2d62da0753b9515c47fb3ffd59d120
Louisza Akinyi <louiszaakinyi@gmail.com>
2024-03-07 11:28:41 +0000

    Merged PR 132: Page fix bug
    `This pull request is for a bug fix for page.tsx`
    
    https://dev.azure.com/MULABS/Bank%20Projects/_workitems/edit/177/
    
    **Type of change**
    
    Please delete options that are not relevant.
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    
    **How Has This Been Tested?**
    
    What tests did you run to verify your changes?
    
    -Test has been done on "../statement/dev"; see attached screenshot
    
    ![image.png](https://dev.azure.com/MULABS/f1790893-6e4c-43a4-8d41-fc1f3b360635/_apis/git/repositories/ec482b3c-c4c3-400a-99de-40c52e2276ca/pullRequests/132/attachments/image.png)
    
    ##### Checklist:
    
    - [x] This review follows the Meraki and Labs Code Review Principal
    - [x] I have performed a self-review of my own code
    - [x] I have made corresponding changes to the documentation. Updated documents are linked below:
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my feature works
    
    - [x] New and existing unit tests pass locally with my changes
    
    Page fix bug
    
    Related work items: #141

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -2,27 +2,14 @@
 import React, { Fragment } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
 import SummaryItem from "@/src/components/widgets/summary-items/summary-items";
-import CurrencySummaryCard from "@/src/components/widgets/currency-summary-card/currency.summary.card";
-<<<<<<< HEAD
-import BalanceInfoCard from "@/src/components/widgets/balance-info-card/balance.info.card";
-import { svgIcon } from "@/src/components/atoms/imageIcon/imageIcon";
-
-=======
 import { ArrowRightOutlined, RetweetOutlined } from "@ant-design/icons";
->>>>>>> 5ad10d72fb5ec778b710a431169f56799e5e44a7
 
 const Dev = () => {
   return (
     <Fragment>
-<<<<<<< HEAD
-        <SummaryItem />
-        <HorizontalInfoDescription title="2.45%" description="This Month"/>
-
-=======
       <div
         style={{ background: "var(--Background-Background-Primary, #F5F5F5)" }}
       >
-        <CurrencySummaryCard></CurrencySummaryCard>
         <SummaryItem
           svgIcon={<RetweetOutlined />}
           summaryTitle="Opening Balance"
@@ -36,7 +23,6 @@ const Dev = () => {
           percentage="2.45%"
           date="This month"
         />
->>>>>>> 5ad10d72fb5ec778b710a431169f56799e5e44a7
       </div>
     </Fragment>
   );
