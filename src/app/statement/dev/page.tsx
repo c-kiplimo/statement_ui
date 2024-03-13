6b7cc5af1b57412599ed2478106c60c9a8155b0f
Stanleyshadrack <stanleyshadrack213@gmail.com>
2024-03-13 16:54:21 +0300

    member-card-item
    changed customer.details.module.css  to customer.info.module.css
    
    member-card-item
    
    changed customer.details.module.css  to customer.info.module.css

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -1,21 +1,32 @@
 "use client";
 import React, { Fragment } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
-import RecentTransactionItem from "@/src/components/widgets/recent-Transaction-Item/recent.transaction.item";
-import { SpotifyOutlined } from "@ant-design/icons";
+import Customerdetails from "@/src/components/widgets/member-card-item/customer-details/customer.info";
+import { RightOutlined } from "@ant-design/icons";
+import FundsAlocation from "@/src/components/widgets/member-card-item/funds-alocation/funds.alocation";
 
 const Dev = () => {
- 
   return (
     <Fragment>
-     <div>
-        <RecentTransactionItem
-          title="Spotify"
-          date="21 Dec, 2023"
-          amount="-49.00"
-          icon={<SpotifyOutlined/>}
-        />
-      </div>
+      <Customerdetails
+        amount="$2,360.00"
+        image="/Ellipse.png"
+        arrow={<RightOutlined />}
+        title="John Doe"
+      />
+
+      <Customerdetails
+        amount="$2,630.00"
+        image="/Ellipse.png"
+        arrow={<RightOutlined />}
+        title="Mira Carder"
+      />
+      <FundsAlocation
+        title="Total Alocated"
+        description="$1,94.00"
+        title2="Total Members"
+        description2="184"
+      />
     </Fragment>
   );
 };
