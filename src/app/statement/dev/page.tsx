b5ccb754e5d6f880ae4496db8b4522f8404d23b8
Louie Mimo <louiemimo@gmail.com>
2024-03-14 09:50:29 +0300

    Adding Cash Flow Widget

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -1,20 +1,39 @@
 "use client";
 import React, { Fragment } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
-import RecentTransactionItem from "@/src/components/widgets/recent-Transaction-Item/recent.transaction.item";
-import { SpotifyOutlined } from "@ant-design/icons";
+import CashFlow from "@/src/components/widgets/cashFlow/cashFlow";
+import { DownloadOutlined } from "@ant-design/icons";
 
 const Dev = () => {
- 
+  const handleCashFlowClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    console.log("CashFlow clicked!");
+  };
   return (
     <Fragment>
-     <div>
-        <RecentTransactionItem
-          title="Spotify"
-          date="21 Dec, 2023"
-          amount="-49.00"
-          icon={<SpotifyOutlined/>}
-        />
+      <div>
+        <CashFlow
+          onClick={handleCashFlowClick}
+          icon={
+            <CashFlow.Icon
+              icon={
+                <DownloadOutlined
+                  size={32}
+                  style={{
+                    color: "white",
+                    backgroundColor: "#84BD00",
+                    padding: "8px",
+                    borderRadius: "50%",
+                  }}
+                />
+              }
+            />
+          }
+          title="Money In"
+          description="$37,890"
+          borderColor="var(--brand-brand-primary)"
+        ></CashFlow>
       </div>
     </Fragment>
   );
