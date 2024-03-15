7a2f75679d30fd05ada187caaa5056ecf73d7c64
Stanleyshadrack <stanleyshadrack213@gmail.com>
2024-03-15 10:24:07 +0300

    member.card.module.css

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -1,25 +1,12 @@
 "use client";
 import React, { Fragment, useState } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
-import ActivePaymentsRequests from "@/src/components/widgets/active-payment-requests/active.payment.requests";
-
+import MemberCard from "@/src/components/widgets/member-card-item/member.card";
 
 const Dev = () => {
-
   return (
     <Fragment>
-      <div className="p-7" style={{backgroundColor:'whitesmoke', width:'100%', height:'100vh'}}>
-      <ActivePaymentsRequests 
-          title={"Active payment request" }
-          description={"You requested $40.00 For Monthly dues"} 
-          image={"/Ellipse.png"} 
-          imageHeight={24} 
-          imageWidth={24} 
-          participants={'283 Participant . Optional . Feb 19,2023'}
-          progressPercent={70} 
-          paidAmount={'$180.14 (32%)'} 
-          goalAmount={'Goal: $420.00'}/>
-      </div>
+      <MemberCard title="Member Card" title2="View more" />
     </Fragment>
   );
 };
