319601d78fc2d325018dca1f95c18eea6613888f
Louie Mimo <louiemimo@gmail.com>
2024-03-18 09:34:54 +0300

    Adding a transaction flow widget

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -1,33 +1,29 @@
 "use client";
 import React, { Fragment } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
-import CashFlow from "@/src/components/widgets/cashFlow/cashFlow";
-import { DownloadOutlined } from "@ant-design/icons";
+import { RiseOutlined } from "@ant-design/icons";
+import TransactionCard from "@/src/components/widgets/transaction-flow/transactionflow";
 
 const Dev = () => {
-  const handleCashFlowClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {};
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log("Widget clicked!");
+  };
   return (
     <Fragment>
       <div>
-        <CashFlow
-          onClick={handleCashFlowClick}
+        <TransactionCard
+          onClick={handleClick}
           icon={
-            <CashFlow.Icon
-              icon={
-                <DownloadOutlined
-                  size={16}
-                  style={{
-                    color: "white",
-                  }}
-                />
-              }
-              iconStyle={{ backgroundColor: " var(--brand-brand-primary)" }}
+            <TransactionCard.Icon
+              icon={<RiseOutlined size={13} />}
+              iconStyle={{ backgroundColor: "#FFF2E0", color: "#FFBD66" }}
             />
           }
-          title="Money In"
-          description="$37,890"></CashFlow>
+          title={"PayPal"}
+          description={"$47,000"}
+          percentage={60}
+          strokeColor={"#FFBD66"}
+        ></TransactionCard>
       </div>
     </Fragment>
   );
---
+++ src/components/widgets/transaction-flow/transactionFlow.tsx
@@ -0,0 +1,98 @@
+import React, { CSSProperties, ReactNode } from "react";
+import styles from "./transactionflow.module.css";
+import { Flex, Progress } from "antd";
+import VerticalInfoDescription from "../../atoms/text/vertical-info-description";
+
+type TransactionProps = {
+  icon: ReactNode;
+  title: string;
+  description: string;
+  percentage: number;
+  strokeColor: string;
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+};
+const TransactionCard = (props: TransactionProps) => {
+  return (
+    <div className={styles.container} onClick={props.onClick}>
+      <div className={styles.info}>
+        <TransactionCard.Icon icon={props.icon} />
+        <TransactionCard.Text
+          title={props.title}
+          titleStyle={{
+            gap: "8px",
+            fontWeight: "400",
+            fontSize: "16px",
+            color: "#6F7269",
+            lineHeight: "24px",
+          }}
+          description={props.description}
+          descriptionStyle={{
+            gap: "8px",
+            fontWeight: "500",
+            fontSize: "20px",
+            color: "#151e00",
+            lineHeight: "32px",
+          }}
+        />
+      </div>
+      <TransactionCard.Progress
+        percentage={props.percentage}
+        strokeColor={props.strokeColor}
+      />
+    </div>
+  );
+};
+
+export default TransactionCard;
+
+type iconProps = {
+  icon: ReactNode;
+  iconStyle?: CSSProperties;
+};
+TransactionCard.Icon = (props: iconProps) => {
+  return (
+    <div className={styles.icon} style={props.iconStyle}>
+      {props.icon}
+    </div>
+  );
+};
+
+type textProps = {
+  title: string;
+  description: string;
+  titleStyle?: CSSProperties;
+  descriptionStyle?: CSSProperties;
+};
+
+TransactionCard.Text = (props: textProps) => {
+  return (
+    <div className={styles.text}>
+      <VerticalInfoDescription
+        title={props.title}
+        description={props.description}
+        titleStyle={props.titleStyle}
+        descriptionStyle={props.descriptionStyle}
+      />
+    </div>
+  );
+};
+
+type progressProps = {
+  percentage: number;
+  strokeColor: string;
+  titleStyle?: CSSProperties;
+  descriptionStyle?: CSSProperties;
+};
+TransactionCard.Progress = (props: progressProps) => {
+  return (
+    <div className={styles.progress}>
+      <Flex vertical>
+        <Progress
+          percent={props.percentage}
+          strokeColor={props.strokeColor}
+          status="active"
+        />
+      </Flex>
+    </div>
+  );
+};
