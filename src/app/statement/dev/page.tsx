099efa18988ac5f172a75deac6a834e5987bc1b6
Louie Mimo <louiemimo@gmail.com>
2024-03-19 10:18:21 +0300

    Commiting refactored widget

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -1,30 +1,41 @@
 "use client";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
-import { RiseOutlined } from "@ant-design/icons";
-import TransactionCard from "@/src/components/widgets/transaction-flow/transactionflow";
+import CashFlowCard from "@/src/components/widgets/cashFlowCard/cashFlowCard";
 
 const Dev = () => {
-  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    console.log("Widget clicked!");
+  const buttons = [
+    { label: "1D" },
+    { label: "7D" },
+    { label: "1M" },
+    { label: "1Y" },
+  ];
+
+  const handleClick: any = () => {};
+  const [activeButton, setActiveButton] = useState<string | null>(null);
+
+  const handleButtonClick = (label: string) => {
+    setActiveButton((prevActiveButton) => {
+      if (prevActiveButton === label) {
+        return null;
+      } else {
+        return label;
+      }
+    });
   };
+
+  const textData = [
+    { text: "Money in", textInfo: "$1,276.00" },
+    { text: "Money out", textInfo: "$4,050.10" },
+  ];
   return (
     <Fragment>
-      <div>
-        <TransactionCard
-          onClick={handleClick}
-          icon={
-            <TransactionCard.Icon
-              icon={<RiseOutlined size={13} />}
-              iconStyle={{ backgroundColor: "#FFF2E0", color: "#FFBD66" }}
-            />
-          }
-          title={"PayPal"}
-          description={"$47,000"}
-          percentage={60}
-          strokeColor={"#FFBD66"}
-        ></TransactionCard>
-      </div>
+      <CashFlowCard
+        title="Cash Flow"
+        buttons={buttons}
+        texts={textData}
+        onButtonClick={handleButtonClick}
+      ></CashFlowCard>
     </Fragment>
   );
 };
