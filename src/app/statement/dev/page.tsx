646c4ebdc8603e73d744c1d380aa2ad5d4685f6e
Stanleyshadrack <stanleyshadrack213@gmail.com>
2024-04-02 16:20:12 +0300

    accountsetup.tsx

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -1,26 +1,26 @@
-// "use client";
-import React, { Fragment, useState } from "react";
+"use client";
+import React, { Fragment } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
 import Accountsetup from "@/src/components/widgets/accountsetup/accountsetup";
 import { SearchOutlined } from "@ant-design/icons";
 
 const Dev = () => {
-  
-
   return (
     <Fragment>
-     <main>
-     {<Accountsetup
-        title={"Provide Account Details"}
-        instruction={
-          "Setup the customer by using account number or customer number"
+      <main>
+        {
+          <Accountsetup
+            title={"Provide Account Details"}
+            instruction={
+              "Setup te customer by using account number or customer number"
+            }
+            option={"Do you want to search by ?"}
+            icon={<SearchOutlined/>}
+            account={"Account Number"}
+            customer={"Customer Number"}
+          />
         }
-        option={"Do you want to search by ?"}
-        icon={<SearchOutlined />}
-        account={"Account Number"}
-        customer={"Customer Number"}
-      /> }
-    </main>
+      </main>
     </Fragment>
   );
 };
