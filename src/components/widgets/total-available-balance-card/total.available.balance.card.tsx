cf1c82e63485acdbe2d03f36936f60add1d02477
Daniel Njoroge <dank8505@gmail.com>
2024-04-03 15:38:35 +0300

    Created available balance card

---
+++ src/components/widgets/total-available-balance-card/total.available.balance.card.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import BalanceInfoCard from "../balance-info-card/balance.info.card";
+import styes from "./total.available.balance.card.module.css";
+
+const balanceArray = [
+  {
+    id: 1,
+    icon: <img src="/KenyanFlagIcon.svg" alt="EuropeIcon" />,
+    countryCurrency: "Kenyan Shillings ",
+    balanceDescription: "Available balance",
+    availableAmount: "354,500.00",
+    currencyCode: "KES",
+  },
+  {
+    id: 2,
+    icon: <img src="/RwandanFlagIcon.svg" alt="EuropeIcon" />,
+    countryCurrency: "Rwandan Francs",
+    balanceDescription: "Available balance",
+    availableAmount: "354,500.00",
+    currencyCode: "RWF",
+  },
+  {
+    id: 3,
+    icon: <img src="/EuropeFlagIcon.svg" alt="EuropeIcon" />,
+    countryCurrency: "European",
+    balanceDescription: "Available balance",
+    availableAmount: "354,500.00",
+    currencyCode: "Euro",
+  },
+  {
+    id: 4,
+    icon: <img src="/UsFlagIcon.svg" alt="EuropeIcon" />,
+    countryCurrency: "US Dollar ",
+    balanceDescription: "Available balance",
+    availableAmount: "354,500.00",
+    currencyCode: "USD",
+  },
+];
+
+const TotalAvailableBalanceCard = () => {
+  return (
+    <div className={`${styes.container} md:w-32`}>
+      {balanceArray.map((balance) => (
+        <div key={balance.id} className="">
+          <BalanceInfoCard
+            svgImage={balance.icon}
+            currencyTitle={balance.countryCurrency}
+            balanceDescription={balance.balanceDescription}
+            availableBalance={balance.availableAmount}
+            currencyCode={balance.currencyCode}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TotalAvailableBalanceCard;
