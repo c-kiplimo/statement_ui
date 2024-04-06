5332ba8594a8927089ab904f0347f6e2483f1074
Daniel Njoroge <dank8505@gmail.com>
2024-04-07 00:11:35 +0300

    created graph

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -1,7 +1,55 @@
 "use client";
 import React, { Fragment} from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
-import CustomerProfile from "@/src/components/widgets/profileForm/profileForm";
+import GraphItem from "@/src/components/widgets/graph-item/graph.item";
+const data = [
+  {
+    name: "Dec",
+    Revenue: 13000,
+    NetIncome: 5000,
+  },
+  {
+    name: "Jan",
+    Revenue: 21000,
+    NetIncome: 9000,
+  },
+  {
+    name: "Feb",
+    Revenue: 20000,
+    NetIncome: 15800,
+  },
+
+  {
+    name: "Mar",
+    Revenue: 22000,
+    NetIncome: 13000,
+  },
+  {
+    name: "Apr",
+    Revenue: 20000,
+    NetIncome: 9000,
+  },
+  {
+    name: "May",
+    Revenue: 21000,
+    NetIncome: 10000,
+  },
+  {
+    name: "Jun",
+    Revenue: 30000,
+    NetIncome: 18000,
+  },
+  {
+    name: "Jul",
+    Revenue: 38000,
+    NetIncome: 20000,
+  },
+  {
+    name: "Aug",
+    Revenue: 32000,
+    NetIncome: 15000,
+  },
+];
 
 const Dev = () => {
 
@@ -23,7 +71,15 @@ const Dev = () => {
   ];
   return (
     <Fragment>
-      <CustomerProfile header="Provide Details to allow us create your profile" headerDesc="We provide ability for you to on board to any country of your choice and ability to switch between different countries" fields={fields}/>
+      <div className="p-9 bg-slate-100">
+        <GraphItem
+          data={data}
+          balanceTitle={"Current Balance"}
+          amount={"$21,850.50"}
+          moneyIntitle={"Money In"}
+          moneyoutTitle={"Money Out"}
+        />
+      </div>
     </Fragment>
   );
 };
