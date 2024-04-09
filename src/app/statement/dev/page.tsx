f59117cd4bfad60ebc99272fc5c882cf2a99fd67
Stanleyshadrack <stanleyshadrack213@gmail.com>
2024-04-09 09:20:50 +0300

    created records

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -1,12 +1,29 @@
 "use client";
-import React, { Fragment} from "react";
+import React, { Fragment } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
-import Usersoverview from "@/src/components/widgets/users-overview/users.overview";
+import Createdrecord from "@/src/components/widgets/account-created-recors-widget/created.record";
 
-const Dev = () => {      
+const Dev = () => {
   return (
     <Fragment>
-      <Usersoverview title={"Users Overview"}/>
+      <Createdrecord
+        hideicon={<img src="/hide.svg" alt="hide" />}
+        editicon={<img src="/edit.svg" alt="edit" />}
+        creationDate={"Date Created"}
+        acctNumber={"Account Number"}
+        numberofTimes={"Frequency"}
+        fileformart={"Template"}
+        acctStatus={"Status"}
+        date={"23-05-2023"}
+        status={"Active"}
+        custName={"Meraki systems tech"}
+        duration={"Daily"}
+        filetype={"PDF"}
+        time={"10:45 a.m"}
+        acctbal={"KES  132314245"}
+        timefrequency={"12.00 A.m"}
+        default={"Default"}
+      />
     </Fragment>
   );
 };
