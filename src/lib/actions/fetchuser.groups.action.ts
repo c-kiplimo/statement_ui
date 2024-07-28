722c0c5fd45cef66913031e5a7ae7f58c965c1c2
Stanleyshadrack <stanleyshadrack213@gmail.com>
2024-07-28 09:59:35 +0300

    select options

---
+++ src/lib/actions/fetchuser.groups.action.ts
@@ -0,0 +1,22 @@
+import PermissionsHandler from "@/src/services/usermanagement/permissions.service";
+export const fetchUserPermissions = async (): Promise<PermissionsSection[]> => {
+  try {
+    const handler = PermissionsHandler();
+    const response: UserPermission[] = await handler.fetchPermissions();
+
+    const permissions: PermissionsSection[] = response.map(data => ({
+      title: data.type,
+      permissions: data.permissions.map(permission => ({
+        permissionName: permission.name,
+        description: permission.description,
+        createdOn: permission.createdAt.split('T')[0],
+        tags: [data.type],
+      }))
+    }));
+
+    return permissions;
+  } catch (error) {
+    console.error('Error fetching user permissions:', error);
+    return [];
+  }
+};
