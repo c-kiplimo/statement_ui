208577d03d395e1d83b7c5cb7951be0be77da3ad
Stanleyshadrack <stanleyshadrack213@gmail.com>
2024-07-30 12:18:11 +0300

    permissions
    permissions

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -3,16 +3,13 @@ import React, { Fragment } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
 import UserGroupsprofile from "@/src/components/widgets/user-groups-profile/user.groups.profile";
 import GroupsUserprofile from "@/src/components/widgets/user-groups-profile/user.groups.profile";
-import { CheckOutlined, FontColorsOutlined } from "@ant-design/icons";
-import AddUserToGroup from "../(protected)/user-management/user-groups/add-user-to-group/addUserToGroup";
-import GroupUsers from "../(protected)/user-management/user-groups/group-users-home-page/group.users";
-import Successful from "@/src/components/widgets/success-widget/successfull/successful";
+import { FontColorsOutlined } from "@ant-design/icons";
+import PermissionAssign from "../(protected)/user-management/user-groups/assign-permission-to-group/permission.assign";
 
 const Dev = () => {
   return (
     <Fragment>
-      <GroupUsers/>
-     
+      <PermissionAssign/>
     </Fragment>
   );
 };
