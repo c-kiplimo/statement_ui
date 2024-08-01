d97b9925869d4de4bb41115969ec969ca5e1b895
Stanleyshadrack <stanleyshadrack213@gmail.com>
2024-08-01 13:15:55 +0300

    code refactor

--- src/app/statement/dev/page.tsx
+++ src/app/statement/dev/page.tsx
@@ -2,12 +2,15 @@
 import React, { Fragment } from "react";
 import withContainer from "../../../components/molecules/shared/statement-core/statement.container.hoc";
 import PermissionsList from "../(protected)/user-management/user-groups/permissionsListPage/permissions.list";
+import { GroupProvider } from "../(protected)/user-management/user-groups/context/permissionsContext";
 
 
 const Dev = () => {
   return (
     <Fragment>
+      <GroupProvider>
       <PermissionsList/>
+      </GroupProvider>
     </Fragment>
   );
 };
